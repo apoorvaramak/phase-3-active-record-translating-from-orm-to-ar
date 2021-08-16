1f27f52fe0bc2d3bb740defd374084fc627c31f5
Apoorva <apoorvar@comcast.net>
2021-08-16 10:14:07 -0400

    commit tree 6fe8a1f624fb97f154e9f7a4e23792340e0a0b98

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,16 @@
-class Dog
+class Dog < ActiveRecord::Base
+    # attr_accessor :name, :breed
+
+    # def initialize(name:, breed:)
+    #     @name = name
+    #     @breed = breed
+    # end
+
+    # def self.create(attributes)
+    #     attributes.each do |key, value|
+    #         self.create("#{key}:", value)
+    #     end
+    #     self.save
+    # end
+
 end
